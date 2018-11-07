e3af1d3c9f480aa077782a88560ada5c3c71f710
Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>
2018-11-06 17:22:17 -0800

    kmssink: Add support for GRAY8 and GRAY10 formats
    This patch adds support for GRAY8 and GRAY10 color formats.
    
    Upstream-status: Pending
    Signed-off-by: Satish Kumar Nagireddy <satish.nagireddy.nagireddy@xilinx.com>

--- sys/kms/gstkmsutils.c
+++ sys/kms/gstkmsutils.c
@@ -31,6 +31,14 @@
 
 #include "gstkmsutils.h"
 
+#ifndef DRM_FORMAT_Y8
+#define DRM_FORMAT_Y8		fourcc_code('G', 'R', 'E', 'Y') /* 8  Greyscale	*/
+#endif
+
+#ifndef DRM_FORMAT_Y10
+#define DRM_FORMAT_Y10		fourcc_code('Y', '1', '0', ' ') /* 10 Greyscale */
+#endif
+
 #ifndef DRM_FORMAT_XV15
 #define DRM_FORMAT_XV15		fourcc_code('X', 'V', '1', '5') /* 2x2 subsampled Cr:Cb plane 2:10:10:10 */
 #endif
@@ -69,6 +77,7 @@ static const struct
   DEF_FMT (P010, P010_10BE),
   DEF_FMT (P016, P016_BE),
 #endif
+  DEF_FMT (Y8, GRAY8),
   DEF_FMT (UYVY, UYVY),
   DEF_FMT (YUYV, YUY2),
   DEF_FMT (YVYU, YVYU),
@@ -80,6 +89,7 @@ static const struct
   DEF_FMT (NV16, NV16),
 #ifdef DRM_FORMAT_XV15
   /* Both formats have been added together */
+  DEF_FMT (Y10, GRAY10_LE32),
   DEF_FMT (XV15, NV12_10LE32),
   DEF_FMT (XV20, NV16_10LE32),
 #endif
