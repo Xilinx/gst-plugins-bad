Xilinx/gst-plugins-bad

commit f350e0c8ae5192892890e7078a40e6d35231d864

kmssink: Create FB on the fly
Creating FB isn't expensive and creating it on the fly will allow
passing per frame flags.

Upsteam-status: Pending
Signed-off-by: Nicolas Dufresne <nicolas.dufresne@collabora.com>

sys/kms/gstkmsallocator.c
@@ -103,18 +103,43 @@ check_fd (GstKMSAllocator * alloc)
 }
 
 static void
+gst_kms_memory_remove_fb (GstMemory * mem)
+{
+  GstKMSAllocator *alloc = GST_KMS_ALLOCATOR (mem->allocator);
+  GstKMSMemory *kmsmem = (GstKMSMemory *) mem;
+
+  if (kmsmem->fb_id) {
+    GST_DEBUG_OBJECT (alloc, "removing fb id %d", kmsmem->fb_id);
+    drmModeRmFB (alloc->priv->fd, kmsmem->fb_id);
+    kmsmem->fb_id = 0;
+  }
+}
+
+static void
 gst_kms_allocator_memory_reset (GstKMSAllocator * allocator, GstKMSMemory * mem)
 {
   int err;
   struct drm_mode_destroy_dumb arg = { 0, };
+  gint i;
 
   if (!check_fd (allocator))
     return;
 
-  if (mem->fb_id) {
-    GST_DEBUG_OBJECT (allocator, "removing fb id %d", mem->fb_id);
-    drmModeRmFB (allocator->priv->fd, mem->fb_id);
-    mem->fb_id = 0;
+  gst_kms_memory_remove_fb ((GstMemory *) mem);
+
+  for (i = 0; i < GST_VIDEO_MAX_PLANES; i++) {
+    struct drm_gem_close arg = { mem->gem_handle[i], };
+    gint err;
+
+    if (mem->gem_handle[i] == 0)
+      continue;
+
+    err = drmIoctl (allocator->priv->fd, DRM_IOCTL_GEM_CLOSE, &arg);
+    if (err)
+      GST_WARNING_OBJECT (allocator, "Failed to close GEM handle: %s %d",
+          strerror (errno), errno);
+
+    mem->gem_handle[i] = 0;
   }
 
   if (!mem->bo)
@@ -208,7 +233,7 @@ gst_kms_allocator_memory_create (GstKMSAllocator * allocator,
      * by the kms driver. */
     pitch = extrapolate_stride (vinfo->finfo, i, arg.pitch);
     GST_VIDEO_INFO_PLANE_STRIDE (vinfo, i) = pitch;
-    GST_VIDEO_INFO_PLANE_OFFSET (vinfo, i) = offs;
+    GST_VIDEO_INFO_PLANE_OFFSET (vinfo, i) = kmsmem->mem_offsets[i] = offs;
 
     /* Note that we cannot negotiate special padding betweem each planes,
      * hence using the display height here. */
@@ -438,18 +463,23 @@ gst_kms_allocator_new (int fd)
 
 /* The mem_offsets are relative to the GstMemory start, unlike the vinfo->offset
  * which are relative to the GstBuffer start. */
-static gboolean
-gst_kms_allocator_add_fb (GstKMSAllocator * alloc, GstKMSMemory * kmsmem,
-    gsize in_offsets[GST_VIDEO_MAX_PLANES], GstVideoInfo * vinfo)
+gboolean
+gst_kms_memory_add_fb (GstMemory * mem, GstVideoInfo * vinfo, guint32 flags)
 {
+  GstKMSAllocator *alloc;
+  GstKMSMemory *kmsmem;
   gint i, ret;
   gint num_planes = GST_VIDEO_INFO_N_PLANES (vinfo);
   guint32 w, h, fmt, bo_handles[4] = { 0, };
   guint32 pitches[4] = { 0, };
   guint32 offsets[4] = { 0, };
 
-  if (kmsmem->fb_id)
-    return TRUE;
+  g_return_val_if_fail (gst_is_kms_memory (mem), FALSE);
+
+  alloc = GST_KMS_ALLOCATOR (mem->allocator);
+  kmsmem = (GstKMSMemory *) mem;
+
+  gst_kms_memory_remove_fb (mem);
 
   w = GST_VIDEO_INFO_WIDTH (vinfo);
   h = GST_VIDEO_INFO_FIELD_HEIGHT (vinfo);
@@ -462,19 +492,24 @@ gst_kms_allocator_add_fb (GstKMSAllocator * alloc, GstKMSMemory * kmsmem,
       bo_handles[i] = kmsmem->gem_handle[i];
 
     pitches[i] = GST_VIDEO_INFO_PLANE_STRIDE (vinfo, i);
-    offsets[i] = in_offsets[i];
+    offsets[i] = kmsmem->mem_offsets[i];
+
+    GST_LOG_OBJECT (alloc, "[%i] pitch %u, offset %u", i, pitches[i],
+        offsets[i]);
   }
 
   GST_DEBUG_OBJECT (alloc, "bo handles: %d, %d, %d, %d", bo_handles[0],
       bo_handles[1], bo_handles[2], bo_handles[3]);
 
   ret = drmModeAddFB2 (alloc->priv->fd, w, h, fmt, bo_handles, pitches,
-      offsets, &kmsmem->fb_id, 0);
+      offsets, &kmsmem->fb_id, flags);
   if (ret) {
     GST_ERROR_OBJECT (alloc, "Failed to bind to framebuffer: %s (%d)",
         strerror (-ret), ret);
     return FALSE;
   }
+
+  GST_DEBUG_OBJECT (alloc, "Create FB %i", kmsmem->fb_id);
 
   return TRUE;
 }
@@ -502,15 +537,7 @@ gst_kms_allocator_bo_alloc (GstAllocator * allocator, GstVideoInfo * vinfo)
   gst_memory_init (mem, GST_MEMORY_FLAG_NO_SHARE, allocator, NULL,
       kmsmem->bo->size, 0, 0, GST_VIDEO_INFO_SIZE (vinfo));
 
-  if (!gst_kms_allocator_add_fb (alloc, kmsmem, vinfo->offset, vinfo))
-    goto fail;
-
   return mem;
-
-  /* ERRORS */
-fail:
-  gst_memory_unref (mem);
-  return NULL;
 }
 
 GstKMSMemory *
@@ -538,21 +565,10 @@ gst_kms_allocator_dmabuf_import (GstAllocator * allocator, gint * prime_fds,
         &kmsmem->gem_handle[i]);
     if (ret)
       goto import_fd_failed;
-  }
 
-  if (!gst_kms_allocator_add_fb (alloc, kmsmem, offsets, vinfo))
-    goto failed;
-
-  for (i = 0; i < n_planes; i++) {
-    struct drm_gem_close arg = { kmsmem->gem_handle[i], };
-    gint err;
-
-    err = drmIoctl (alloc->priv->fd, DRM_IOCTL_GEM_CLOSE, &arg);
-    if (err)
-      GST_WARNING_OBJECT (allocator,
-          "Failed to close GEM handle: %s %d", strerror (errno), errno);
-
-    kmsmem->gem_handle[i] = 0;
+    /* This is per memory offset, in contrast with the vinfo offsets which are
+     * relative to the GstBuffer layout. */
+    kmsmem->mem_offsets[i] = offsets[i];
   }
 
   return kmsmem;
@@ -562,11 +578,6 @@ import_fd_failed:
   {
     GST_ERROR_OBJECT (alloc, "Failed to import prime fd %d: %s (%d)",
         prime_fds[i], strerror (-ret), ret);
-    /* fallback */
-  }
-
-failed:
-  {
     gst_memory_unref (mem);
     return NULL;
   }

sys/kms/gstkmsallocator.h
@@ -57,6 +57,7 @@ struct _GstKMSMemory
 
   guint32 fb_id;
   guint32 gem_handle[GST_VIDEO_MAX_PLANES];
+  gsize mem_offsets[GST_VIDEO_MAX_PLANES];
   struct kms_bo *bo;
 };
 
@@ -72,8 +73,11 @@ struct _GstKMSAllocatorClass {
 
 GType gst_kms_allocator_get_type (void) G_GNUC_CONST;
 
-gboolean gst_is_kms_memory (GstMemory *mem);
-guint32 gst_kms_memory_get_fb_id (GstMemory *mem);
+gboolean gst_is_kms_memory        (GstMemory *mem);
+guint32  gst_kms_memory_get_fb_id (GstMemory *mem);
+gboolean gst_kms_memory_add_fb    (GstMemory * kmsmem,
+                                   GstVideoInfo * vinfo,
+                                   guint32 flags);
 
 GstAllocator* gst_kms_allocator_new (gint fd);
 

sys/kms/gstkmssink.c
@@ -476,12 +476,11 @@ configure_mode_setting (GstKMSSink * self, GstVideoInfo * vinfo)
   drmModeModeInfo *mode = NULL;
   drmModeModeInfo *cached_mode = NULL;
   guint32 fb_id;
-  GstKMSMemory *kmsmem;
+  GstMemory *mem = 0;
   guint fps;
   ret = FALSE;
   conn = NULL;
   mode = NULL;
-  kmsmem = NULL;
 
   if (self->vinfo_crtc.finfo
       && gst_video_info_is_equal (&self->vinfo_crtc, vinfo))
@@ -492,10 +491,14 @@ configure_mode_setting (GstKMSSink * self, GstVideoInfo * vinfo)
 
   GST_INFO_OBJECT (self, "configuring mode setting");
 
-  kmsmem = (GstKMSMemory *) gst_kms_allocator_bo_alloc (self->allocator, vinfo);
-  if (!kmsmem)
+  mem = gst_kms_allocator_bo_alloc (self->allocator, vinfo);
+  if (!mem)
     goto bo_failed;
-  fb_id = kmsmem->fb_id;
+
+  if (!gst_kms_memory_add_fb (mem, vinfo, 0))
+    goto bo_failed;
+
+  fb_id = gst_kms_memory_get_fb_id (mem);
 
   conn = drmModeGetConnector (self->fd, self->conn_id);
   if (!conn)
@@ -546,7 +549,8 @@ configure_mode_setting (GstKMSSink * self, GstVideoInfo * vinfo)
     goto modesetting_failed;
 
   g_clear_pointer (&self->tmp_kmsmem, gst_memory_unref);
-  self->tmp_kmsmem = (GstMemory *) kmsmem;
+  self->tmp_kmsmem = mem;
+  mem = NULL;
   self->vinfo_crtc = *vinfo;
 
   ret = TRUE;
@@ -554,6 +558,8 @@ configure_mode_setting (GstKMSSink * self, GstVideoInfo * vinfo)
 bail:
   if (conn)
     drmModeFreeConnector (conn);
+  if (mem)
+    gst_memory_unref (mem);
 
   return ret;
 
@@ -1617,8 +1623,7 @@ gst_kms_sink_import_dmabuf (GstKMSSink * self, GstBuffer * inbuf,
 
   kmsmem = (GstKMSMemory *) gst_kms_allocator_get_cached (mems[0]);
   if (kmsmem) {
-    GST_LOG_OBJECT (self, "found KMS mem %p in DMABuf mem %p with fb id = %d",
-        kmsmem, mems[0], kmsmem->fb_id);
+    GST_LOG_OBJECT (self, "found KMS mem %p in DMABuf mem %p", kmsmem, mems[0]);
     goto wrap_mem;
   }
 
@@ -1746,6 +1751,7 @@ gst_kms_sink_show_frame (GstVideoSink * vsink, GstBuffer * buf)
 {
   gint ret;
   GstBuffer *buffer = NULL;
+  GstMemory *mem;
   guint32 fb_id;
   GstKMSSink *self;
   GstVideoCropMeta *crop;
@@ -1768,7 +1774,12 @@ gst_kms_sink_show_frame (GstVideoSink * vsink, GstBuffer * buf)
 
   if (!buffer)
     return GST_FLOW_ERROR;
-  fb_id = gst_kms_memory_get_fb_id (gst_buffer_peek_memory (buffer, 0));
+
+  mem = gst_buffer_peek_memory (buffer, 0);
+  if (!gst_kms_memory_add_fb (mem, &self->vinfo, 0))
+    goto buffer_invalid;
+
+  fb_id = gst_kms_memory_get_fb_id (mem);
   if (fb_id == 0)
     goto buffer_invalid;
 

commit 458cf6f7484302b50743afc738c84f01369283e1

kmssink: Update pitch value from stride of incoming dmabuf
While importing dmabuf, update pitch value for each of the planes
in kmsmem struct from stride metadata attached to the incoming
dmabuf.

Note: This patch along with
f76cde5ea2adcf4f034b487ee7385006d2858974:
kmssink: Use driver provided pitch for creating framebuffer object

fixes the regression introduced in
f350e0c8ae5192892890e7078a40e6d35231d864: kmssink: Create FB on the fly

Upstream-Status: Pending
Signed-off-by: Devarsh Thakkar <devarsh.thakkar@xilinx.com>

sys/kms/gstkmsallocator.c
@@ -582,6 +582,7 @@ gst_kms_allocator_dmabuf_import (GstAllocator * allocator, gint * prime_fds,
     if (ret)
       goto import_fd_failed;
 
+    kmsmem->pitches[i] = GST_VIDEO_INFO_PLANE_STRIDE (vinfo, i);
     /* This is per memory offset, in contrast with the vinfo offsets which are
      * relative to the GstBuffer layout. */
     kmsmem->mem_offsets[i] = offsets[i];