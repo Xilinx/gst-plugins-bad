347902eb45da63499323cdda46ffc08417d91737
Devarsh Thakkar <devarsh.thakkar@xilinx.com>
2021-04-19 04:56:27 -0700

    sys: kms: Get correct bpp and width for grey formats
    Return correct bitsperpixel and width for 8bit and 10bit
    grey formats.
    
    Upstream Status: Pending
    Signed-off-by: Devarsh Thakkar <devarsh.thakkar@xilinx.com>
    Signed-off-by: Jeegar Patel <jeegar.patel@xilinx.com>
    
    Acked-for-series:Varunkumar Allagadapa <varunkumar.allagadapa@xilinx.com>

--- sys/kms/gstkmsutils.c
+++ sys/kms/gstkmsutils.c
@@ -132,11 +132,13 @@ gst_drm_bpp_from_drm (guint32 drmfmt)
     case DRM_FORMAT_NV12:
     case DRM_FORMAT_NV21:
     case DRM_FORMAT_NV16:
+    case DRM_FORMAT_Y8:
       bpp = 8;
       break;
 #ifdef DRM_FORMAT_XV15
     case DRM_FORMAT_XV15:
     case DRM_FORMAT_XV20:
+    case DRM_FORMAT_Y10:
       /* One 32b macro pixel: three 10b pixels + 2b padding */
       bpp = 32;
       break;
@@ -174,6 +176,7 @@ gst_drm_width_from_drm (guint32 drmfmt, guint32 width)
       break;
 #ifdef DRM_FORMAT_XV15
       /* Convert pixel width to macropixel width */
+    case DRM_FORMAT_Y10:
     case DRM_FORMAT_XV15:
     case DRM_FORMAT_XV20:
       ret = gst_util_uint64_scale_round (width, 1, 3);
